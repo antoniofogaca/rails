ec366e20412be18a2bd383f5762581fe8dddb65f
Antonio Valdeci Fogaca <antoniovaldeci@sefin.ro.gov.br>
2019-09-05 13:16:19 -0400

    Atualização do curso via sefin

--- app/controllers/funcionarios_controller.rb
+++ app/controllers/funcionarios_controller.rb
@@ -4,7 +4,7 @@ class FuncionariosController < ApplicationController
   # GET /funcionarios
   # GET /funcionarios.json
   def index
-    @funcionarios = Funcionario.where("cpf like ?", "%#{params[:filtro_descricao]}%")
+    @funcionarios = Funcionario.where(" nome like ?", "%#{params[:filtro_descricao]}%") 
   end
 
   # GET /funcionarios/1
---
+++ teste.rb
@@ -0,0 +1,66 @@
+# if
+
+# puts 'Informe o valor x:'
+# x = gets.chomp.to_i
+# if x > 2
+#     puts 'x > 2'
+# else
+#     puts 'x < 2'    
+# end
+
+# unless
+
+# puts 'Informe o valor de a:'
+# a = gets.chomp.to_i
+# unless a >= 2
+#     puts 'a menor que 2'
+# else
+#     puts 'a maior que 2'
+# end   
+
+# case
+
+# print 'Digite a idade:'
+# idade = gets.chomp.to_i
+
+# case idade
+# when 0..2
+#     puts 'Bebê'
+# when 3..12
+#     puts 'Criança'
+# when 13..18
+#     puts 'Adolescente'    
+# else
+#     puts 'Adulto'
+# end
+
+# sexo = 'F'
+# puts (sexo == 'M'? 'Masculino' : 'Feminino')
+
+# sexo = 'F'
+# puts sexo == 'M'? (puts 'Masculino') : (puts'Feminino')
+
+# while do
+
+# i = 0
+# n = 5
+# while i < n do
+#     puts "contando..." +i.to_s
+#     i += 1
+# end    
+
+# each - range (...)
+
+# (0...5).each do |i|
+#     puts "O valor lido foi: " + i.to_s
+# end    
+
+# Array com each
+
+# ['a','b',3].each do |i|
+#     puts "O valor foi:" + i.to_s
+#     if i == 3
+#         a = i + 4
+#         puts 'posição do array: ' + i.to_s + " + a = " + a.to_s 
+#     end
+# end    
