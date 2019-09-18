7873f8f1ee296ba3afa1dd8fef20bf6bd36973db
Antonio Valdeci Fogaca <antoniovaldeci@sefin.ro.gov.br>
2019-09-18 09:40:46 -0400

    sefin rails

--- modulo/consumoApp.rb
+++ modulo/consumoApp.rb
@@ -2,7 +2,7 @@ require 'rest-client'
 require 'json'
 require 'net/http'
 
-# Consumo api via GET
+# Consumo api via POST
 
 req = RestClient::Request.execute(
    :method => :post,
@@ -20,14 +20,6 @@ req = RestClient::Request.execute(
 # )
 
 # Consumo api via POST
-
-# req = RestClient::Request.execute(
-#    :method => :post,
-#    :verify_ssl => true,
-#    :url => 'https://api.desenv.bpm.sefin.ro.gov.br/v1/relatorios/acaofiscal/designacaofiscal/20162500100084',
-#    :headers => { :'access-key' => 'b9be11166d72e9e3ae7fd407165e4bd2', :'secret-key' => '29481bf5d996d39610e57c0254ee33b7', :'Content-Type' => "application/x-www-form-urlencoded"}
-#    :'numero_auto' => "20192702800017", :'matricula' => "0300149552", :'situacao' => "013" 
-# )
 
 json = JSON.parse(req.body)
 
