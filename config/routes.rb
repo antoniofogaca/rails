ddd3e4cb7b0f16b08afbac1dda4924e4ba0316a9
Antonio Valdeci Fogaca <antoniovaldeci@sefin.ro.gov.br>
2019-10-07 13:08:25 -0400

    sefin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  get  'funcionarios/index'
   resources :funcionarios
   resources :appwebexemps
+  
+  root to: 'funcionarios#index'
+
+
+  
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
